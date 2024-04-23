c32122469bb10a93d83ab339e941e71a02fda9b1
nati-rc <natalia.ramirez.6@gmail.com>
2024-04-23 16:01:18 -0700

    Feat - Added an API to receive the audio file and JSON encoded the response

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+from flask import Flask, request, Response, jsonify
+import io
 import librosa
 from model_utils import load_model
 from analysis import analyze_segments
 import csv
 from label_groups import label_groups, special_labels
+import numpy as np
+import json
 
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()  # Convert numpy arrays to lists
+        return super().default(obj)
+
+app = Flask(__name__)
 # Constants
 model_url = 'https://www.kaggle.com/models/google/yamnet/tensorFlow2/yamnet/1'
-audio_path = 'amadeus-bark.wav'
 
-# Load model and audio
+# Load model
 model = load_model(model_url)
-audio, sampling_rate = librosa.load(audio_path, sr=16000)
 
 # Load class names
 class_names = []
@@ -20,17 +33,25 @@ with open('yamnet_class_map.csv', 'r') as f:
     for row in reader:
         class_names.append(row[2])
 
-# Analyze audio
-intervals = librosa.effects.split(audio, top_db=40)  # Example threshold
-target_length = 16000  # One second of audio, defined in main.py
-# Define thresholds
-min_rms_threshold = 0.01
-min_pitch_prob_threshold = 0.005
-results = analyze_segments(audio, intervals, sampling_rate, class_names, label_groups, special_labels, model, target_length, min_rms_threshold, min_pitch_prob_threshold)
+@app.route('/analyze', methods=['POST'])
+def analyze_endpoint():
+    if 'audio_file' not in request.files:
+        return jsonify({'error': 'Missing audio file'}), 400
 
+    audio_file = request.files['audio_file']
+    audio_file_in_memory = io.BytesIO(audio_file.read())
+    audio, sr = librosa.load(audio_file_in_memory, sr=16000)  # Load directly with desired sample rate
 
-# Output results
-#print(results)
+    # Define thresholds and target length for audio processing
+    min_rms_threshold = 0.01
+    min_pitch_prob_threshold = 0.005
+    target_length = 16000  # One second of audio
 
-for start_time, end_time, category, score in results:
-    print(f"Segment: Start {start_time:.2f}s, End {end_time:.2f}s, Category: {category}, Score: {score:.4f}")
+    intervals = librosa.effects.split(audio, top_db=40)  # Example threshold
+    results = analyze_segments(audio, intervals, sr, class_names, label_groups, special_labels, model, target_length, min_rms_threshold, min_pitch_prob_threshold)
+    results = json.dumps(results, cls=CustomJSONEncoder)
+
+    return Response(results, mimetype='application/json')  # Return analysis results in json format
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Turn off debug in production
